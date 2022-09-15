023535333a0c8105c09bef19ed1c7830b3cde140
Saqlain <iamkhatik786@gmail.com>
2022-09-15 11:35:39 +0530

    Stock exchange management

---
+++ StockAccountManagement.java
@@ -0,0 +1,13 @@
+package com.bl.stockaccountmanagement;
+
+public class StockAccountManagement {
+
+	public static void main(String[] args) {
+		
+		System.out.println("******Welcome to the Stock Account Management Program******");
+		
+		StockPortfolio portfolio = new StockPortfolio();
+		portfolio.selectChoice();
+		
+	}
+}
